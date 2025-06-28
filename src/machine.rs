93c7e95443e190b1f83e1927307e9ef8dc9c9b32
Adam Eury <euryadam@gmail.com>
2025-06-28 13:44:42 -0400

    Remove strlen test

--- src/machine.rs
+++ src/machine.rs
@@ -403,29 +403,4 @@ mod tests {
         let got = machine.regs.get(Reg::a0);
         assert_eq!(want, got, "wrong a0: {want}, expected: {got}");
     }
-
-    #[test]
-    #[ignore]
-    fn strlen_program_counts_characters_in_a_string() {
-        let program = r#"
-            la     a0, mystr     # Load the address of mystr into a0
-            li     t0, 0         # i = 0
-        loop: # Start of for loop
-            add    t1, t0, a0    # Add the byte offset for str[i]
-            lb     t1, 0(t1)     # Dereference str[i]
-            beq    t1, zero, end # if str[i] == 0, break for loop
-            addi   t0, t0, 1     # Add 1 to our iterator
-            add    t1, t0, a0
-            j      loop          # Jump back to condition (1 backwards)
-        end: # End of for loop
-            ebreak
-        mystr:
-            .ascii "test\0"
-        "#;
-
-        let image = asm::assemble(program).unwrap();
-
-        let mut machine = Machine::new();
-        machine.load_image(&image);
-    }
 }
